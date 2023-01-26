e6f430263bdefee5520c03ec7af8b04ea6994206
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-01-26 10:48:42 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -1,10 +1,6 @@
 const fs = require("fs");
 
-const inputData = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split(" ")
-  .map(Number);
+const inputData = fs.readFileSync("/dev/stdin").toString().split(" ");
 
 const A = Number(inputData[0]);
 const B = Number(inputData[1]);

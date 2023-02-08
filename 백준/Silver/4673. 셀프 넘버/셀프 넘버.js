c4bde7c65f84ceb6753cda26b17a5f4a8f64a120
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-02-08 18:11:07 +0900

    [Silver V] Title: 셀프 넘버, Time: 236 ms, Memory: 10484 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -0,0 +1,20 @@
+function d(iD) {
+  let constr = [];
+  for (let j = 1; j < iD; j++) {
+    let iD = j;
+    let con = 0;
+    let stringNum = iD.toString();
+    for (let i = 0; i < stringNum.length; i++) {
+      con += Number(stringNum[i]);
+    }
+    con += iD;
+    constr.push(con);
+  }
+  let maxNum = Math.max(...constr);
+  let selfNum = Array.from({ length: maxNum }, (v, i) => i + 1);
+  let difference = selfNum.filter((x) => !constr.includes(x));
+  let diff2 = difference.filter((x) => x < iD);
+
+  console.log(diff2.join("\n"));
+}
+d(10000);

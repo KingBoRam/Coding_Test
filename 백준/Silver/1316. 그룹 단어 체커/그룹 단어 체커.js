972aa6ab27aebde109ac08eff063580ec28df0ab
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-02-19 17:00:30 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 184 ms, Memory: 9408 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const iD = fs.readFileSync(0).toString().trim().split("\n");
+let count = Number(iD[0]);
+let num = 0;
+for (let i = 1; i <= count; i++) {
+  let A = iD[i];
+  let arr = [];
+  for (let j = 0; j < A.length; j++) {
+    if (!arr.includes(A[j])) {
+      arr.push(A[j]);
+    }
+    if (A[j] == A[j - 1]) {
+      arr.push(A[j]);
+    }
+  }
+  if (A === arr.join("")) {
+    num++;
+  }
+}
+console.log(num);

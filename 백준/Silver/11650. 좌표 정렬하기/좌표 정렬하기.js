f2afc19be410cc6f4945ced3c7dfca001c8e0ceb
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-03-29 11:14:54 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 548 ms, Memory: 58232 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const iD = fs.readFileSync(0).toString().trim().split("\n");
+const count = +iD[0];
+iD.shift();
+
+const arr = [];
+for (let i = 0; i < count; i++) {
+  arr.push(iD[i].toString().trim().split(" "));
+}
+
+arr.sort((a, b) => {
+  if (a[0] !== b[0]) {
+    return a[0] - b[0];
+  } else if (a[0] === b[0]) {
+    return a[1] - b[1];
+  }
+});
+
+const result = [];
+for (let i = 0; i < count; i++) {
+  let answer = arr[i].join(" ");
+  result.push(answer);
+}
+
+console.log(result.join("\n"));

fb2fcd74eb376ab97d49f99c272b7787172fc3db
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-04 10:53:25 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.js
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
-
-const inputData = fs.readFileSync("/dev/stdin").toString().split(" ");
-
-const A = Number(inputData[0]);
-const B = Number(inputData[1]);
-
-console.log(A + B);
-console.log(A - B);
-console.log(A * B);
-console.log(Math.floor(A / B));
-console.log(A % B);
+const iD = fs
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+const A = iD[0];
+const B = iD[1];
+const result = [A + B, A - B, A * B, Math.floor(A / B), A % B];
+console.log(result.join("\n"));

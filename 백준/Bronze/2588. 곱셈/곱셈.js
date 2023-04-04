33230d022d4b16019fed76102391859294cfd481
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-04 11:12:58 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 9572 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -1,13 +1,8 @@
 const fs = require("fs");
-
-const [inputData1, inputData2] = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split("\n")
-  .map(Number);
-
-const reverseData = inputData2.toString().split("").reverse().map(Number);
-const resultData = reverseData.map((value) => value * inputData1);
-resultData.push(inputData1 * inputData2);
-
-console.log(resultData.join("\n"));
+const [A, B] = fs.readFileSync(0).toString().trim().split("\n");
+const third = parseInt(A) * parseInt(B[2]);
+const forth = parseInt(A) * parseInt(B[1]);
+const fifth = parseInt(A) * parseInt(B[0]);
+const sixth = parseInt(A) * parseInt(B);
+const result = [third, forth, fifth, sixth];
+console.log(result.join("\n"));

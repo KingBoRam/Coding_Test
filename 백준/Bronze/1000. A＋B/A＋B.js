e2f34bae77dd652b0bce7ff3193d260ac701e1e3
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-04 10:22:49 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
-
-const inputData = fs.readFileSync("/dev/stdin").toString().split(" ");
-
-const A = parseInt(inputData[0]);
-const B = parseInt(inputData[1]);
-
-console.log(A + B);
+const iD = fs
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+const result = iD[0] + iD[1];
+console.log(result);

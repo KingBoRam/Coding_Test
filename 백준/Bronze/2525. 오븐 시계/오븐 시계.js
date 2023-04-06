d368836650d9b7fa873cc58a9b102b034c4a7551
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-06 11:37:15 +0900

    [Bronze III] Title: 오븐 시계, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -1,22 +1,15 @@
 const fs = require("fs");
+const iD = fs.readFileSync(0).toString().trim().split("\n");
+const [H, M] = iD[0].toString().trim().split(" ").map(Number);
+const M2 = +iD[1];
 
-const inputData = fs.readFileSync(0).toString().split("\n");
-
-let A = Number(inputData[0].split(" ")[0]);
-let B = Number(inputData[0].split(" ")[1]);
-let C = Number(inputData[1]);
-let D = Math.floor((B + C) / 60);
-
-if (B + C >= 60) {
-  if (A + D > 23) {
-    A = A + D - 24;
-    B = B + C - D * 60;
+if (M + M2 < 60) {
+  console.log(H + " " + (M + M2));
+} else if (M + M2 >= 60) {
+  let H2 = Math.floor((M + M2) / 60);
+  if (H + H2 >= 24) {
+    console.log(H + H2 - 24 + " " + ((M + M2) % 60));
   } else {
-    A = A + D;
-    B = B + C - D * 60;
+    console.log(H + H2 + " " + ((M + M2) % 60));
   }
-  console.log(A + " " + B);
-} else {
-  B = B + C;
-  console.log(A + " " + B);
 }

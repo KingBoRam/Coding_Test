6a679323cbb19648769bb163f6abd41d68e2743a
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-07 16:22:50 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 9508 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -1,13 +1,8 @@
 const fs = require("fs");
+const iD = fs.readFileSync(0).toString().trim().split("\n");
+const count = +iD.shift();
 
-const iD = fs.readFileSync(0).toString().split("\n");
-
-const count = Number(iD[0]);
-
-let result = "";
-
-for (i = 1; i <= count; i++) {
-  let ex = iD[i].split(" ");
-  result += "Case #" + i + ": " + (Number(ex[0]) + Number(ex[1])) + "\n";
+for (let i = 0; i < count; i++) {
+  const arr = iD[i].toString().trim().split(" ").map(Number);
+  console.log(`Case #${i + 1}: ${arr[0] + arr[1]}`);
 }
-console.log(result);

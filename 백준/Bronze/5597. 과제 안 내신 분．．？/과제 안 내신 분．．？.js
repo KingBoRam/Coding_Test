2b57a7821af2f97610d59abcf0e11693fd4c7910
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-10 12:16:50 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
-
-const iD = fs.readFileSync(0).toString().split("\n").map(Number);
-
-let iD2 = Array.from({ length: 30 }, (v, i) => i + 1);
-
-let difference = iD2.filter((x) => !iD.includes(x));
-
-console.log(difference.join("\n"));
+const iD = fs
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+const result = [];
+for (let i = 1; i <= 30; i++) {
+  if (!iD.includes(i)) {
+    result.push(i);
+  }
+}
+console.log(result.join("\n"));

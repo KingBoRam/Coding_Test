7fc8ec65e420e980efc51f1dd197fb05fb66ad19
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-10 12:05:18 +0900

    [Bronze III] Title: 공 넣기, Time: 136 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.js
+++ 백준/Bronze/10810. 공 넣기/공 넣기.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
-const iD = fs.readFileSync(0).toString().split("\n");
-let iD2 = iD[0].split(" ").map(Number);
-let basket = Array.from({ length: iD2[0] }, (v) => 0);
+const iD = fs.readFileSync(0).toString().trim().split("\n");
+const [M, N] = iD[0].split(" ");
+const arr = Array.from({ length: M }).fill(0);
 
-for (let i = 1; i <= iD2[1]; i++) {
-  let arr = iD[i].split(" ").map(Number);
-  for (let j = arr[0] - 1; j <= arr[1] - 1; j++) {
-    basket[j] = arr[2];
+for (let i = 1; i <= N; i++) {
+  const line = iD[i].toString().trim().split(" ");
+  for (let j = line[0] - 1; j <= line[1] - 1; j++) {
+    arr[j] = line[2];
   }
 }
-console.log(basket.join(" "));
+console.log(arr.join(" "));

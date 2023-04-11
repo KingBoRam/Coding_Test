4e23ae8f33e4d17e8049807728cb44d8d484dd56
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-11 18:08:48 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 132 ms, Memory: 9492 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
-
 const iD = fs.readFileSync(0).toString().trim().split("\n");
-
 for (let i = 0; i < iD.length; i++) {
-  let str = iD[i].trim();
-  console.log(str);
+  const arr = iD[i].trim();
+  console.log(arr);
 }

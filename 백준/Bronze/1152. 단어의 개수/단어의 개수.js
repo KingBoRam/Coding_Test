958270bfe83a97f15ebb7c5f1a38e27e9c2311eb
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-11 17:48:33 +0900

    [Bronze II] Title: 단어의 개수, Time: 144 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,12 +1,7 @@
 const fs = require("fs");
-
 const iD = fs.readFileSync(0).toString().trim().split(" ");
-
-let count = 0;
-for (let i = 0; i < iD.length; i++) {
-  if (iD[i].length > 0) {
-    count++;
-  }
+if (iD[0] === "") {
+  console.log(0);
+} else {
+  console.log(iD.length);
 }
-
-console.log(count);

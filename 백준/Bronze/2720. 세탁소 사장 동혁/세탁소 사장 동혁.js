0efc11ce0272163578fe66cbf3ea3441eef99e4c
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-17 16:33:31 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 156 ms, Memory: 10428 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.js
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.js
@@ -5,12 +5,9 @@ const iD = fs
   .trim()
   .split("\n")
   .map(Number);
-const count = +iD[0];
-iD.shift();
-
 const [quarter, dime, nickel, penny] = [25, 10, 5, 1];
 
-for (let i = 0; i < count; i++) {
+for (let i = 1; i <= iD[0]; i++) {
   let arr = [];
   let num = iD[i];
   arr.push(parseInt(num / quarter));
@@ -22,4 +19,3 @@ for (let i = 0; i < count; i++) {
   arr.push(parseInt(num / penny));
   console.log(arr.join(" "));
 }
-

33c808f1fe3dd80345e8e84ad38a7d9d4592b0d9
KingBoRam <107662593+KingBoRam@users.noreply.github.com>
2023-04-20 18:10:33 +0900

    [Silver IV] Title: 괄호, Time: 140 ms, Memory: 9564 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -4,16 +4,12 @@ const count = +iD.shift();
 
 for (let i = 0; i < count; i++) {
   let arr = iD[i].trim();
-  if (arr.length % 2 !== 0) {
+  while (arr.includes("()")) {
+    arr = arr.replace("()", "");
+  }
+  if (arr.length > 0) {
     console.log("NO");
-  } else if (arr.length % 2 === 0) {
-    while (arr.includes("()")) {
-      arr = arr.replace("()", "");
-    }
-    if (arr.length > 0) {
-      console.log("NO");
-    } else {
-      console.log("YES");
-    }
+  } else {
+    console.log("YES");
   }
 }
